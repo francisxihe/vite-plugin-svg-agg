969a832622790418f84fa70c4b249720aaaae14a
xuwh <xuwenhua@XWH-MacBook-Pro.local>
2024-03-13 16:56:28 +0800

    init

---
+++ apps/playground/vite.config.ts
@@ -0,0 +1,20 @@
+import { UserConfigExport } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import jsx from '@vitejs/plugin-vue-jsx'
+import { createSvgIconsPlugin } from 'vite-plugin-svg-agg'
+import path from 'path'
+
+export default (): UserConfigExport => {
+  return {
+    plugins: [
+      vue(),
+      jsx(),
+      createSvgIconsPlugin({
+        iconDirs: [path.resolve(process.cwd(), 'src/icons')],
+        // icon symbolId
+        // default
+        symbolId: 'icon-[dir]-[name]',
+      }),
+    ],
+  }
+}
